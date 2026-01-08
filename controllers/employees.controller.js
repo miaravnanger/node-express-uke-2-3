1b7c6af575c4843937c98373a1b80b66eaa74b21
miaravnanger <mia_maroen@hotmail.com>
2026-01-08 14:36:43 +0100

    notater time 08.01

--- controllers/employees.controller.js
+++ controllers/employees.controller.js
@@ -1,4 +1,5 @@
 const { readJsonDB, writeJsonDB } = require("../util");
+const { v4: uuid } = require("uuid");
 
 // Hent ut alle ansatte fra DB (JSON)
 function getEmployees() {
@@ -7,32 +8,119 @@ function getEmployees() {
   return employees;
 }
 
+// Hent ut én ansatt fra DB (JSON)
 function getEmployeeById(id) {
   const employees = readJsonDB("employees");
   const index = employees.map((e) => e.id).indexOf(id);
 
   if (index === -1) {
-    throw new Error(`Employee with id ${id} doesn't exist!`, { status: 404 });
+    throw new Error(`Employee with id ${id} doesn't exist!`, { cause: 404 });
   }
 
   return employees[index];
 }
 
+// Opprett en ansatt
 function createEmployee(employeeData) {
   delete employeeData.id;
 
-  // ... validering ...
+  // ... inndatavalidering ...
 
   const employees = readJsonDB("employees");
-  employeeData.id = employees[employees.length - 1].id + 1;
+
+  employeeData.id = uuid();
   employees.push(employeeData);
+
   writeJsonDB("employees", JSON.stringify(employees));
 
   return { success: true, _inserted: employeeData };
+}
+
+// Update / PATCH
+function updateEmployee(id, updatedEmployeeData) {
+  const employees = readJsonDB("employees");
+  const index = employees.map((e) => e.id).indexOf(id);
+
+  if (index === -1) {
+    throw new Error(`Employee with id ${id} doesn't exist!`, { cause: 404 });
+  }
+
+  // ... Inndatavalidering ...
+
+  delete updatedEmployeeData.id;
+
+  const _old = { ...employees[index] };
+  const employee = { ...employees[index], ...updatedEmployeeData };
+  employees[index] = employee;
+
+  writeJsonDB("employees", JSON.stringify(employees));
+
+  return { success: true, _updated: employee, _old };
+}
+
+// Upsert (update & insert) / PUT
+function upsertEmployee(id, upsertedEmployeeData) {
+  const employees = readJsonDB("employees");
+  const index = employees.map((e) => e.id).indexOf(id);
+
+  // ... Inndatavalidering ...
+
+  if (upsertedEmployeeData.id && id !== upsertedEmployeeData.id) {
+    throw new Error(
+      `id parameter doesn't match id of body: ${id} !== ${upsertedEmployeeData.id}`,
+      { cause: 400 }
+    );
+  }
+
+  if (index === -1) {
+    // Eksisterer ikke, så vi setter inn den nye dataen i DB
+    if (!upsertedEmployeeData.id) {
+      throw new Error(`id parameter missing from body!`);
+    }
+
+    employees.push(upsertedEmployeeData);
+    writeJsonDB("employees", JSON.stringify(employees));
+    return { success: true, status: 201, _inserted: upsertedEmployeeData };
+  }
+
+  // Ansatt eksisterer, så vi oppdaterer dataen
+
+  delete upsertedEmployeeData.id;
+
+  const employee = { ...employees[index], ...upsertedEmployeeData };
+  employees[index] = employee;
+
+  writeJsonDB("employees", JSON.stringify(employees));
+
+  return {
+    success: true,
+    status: 200,
+    _updated: { id, ...upsertedEmployeeData },
+  };
+}
+
+// DELETE
+function deleteEmployee(id) {
+  const employees = readJsonDB("employees");
+  const index = employees.map((e) => e.id).indexOf(id);
+
+  if (index === -1) {
+    // Eksisterer ikke, send 404
+    throw new Error(`Employee with id ${id} doesn't exist!`, { cause: 404 });
+  }
+
+  employees.splice(index, 1);
+
+  writeJsonDB("employees", JSON.stringify(employees));
+
+  return { success: true, status: 204, _deletedId: id };
 }
 
 module.exports = {
   getEmployees,
   getEmployeeById,
   createEmployee,
+  updateEmployee,
+  upsertEmployee,
+  deleteEmployee,
 };
--- routes/employees.route.js
+++ routes/employees.route.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
-const { readJsonDB, writeJsonDB } = require("../util");
 const {
   getEmployees,
   getEmployeeById,
   createEmployee,
+  deleteEmployee,
+  updateEmployee,
+  upsertEmployee,
 } = require("../controllers/employees.controller");
 
 const employeesRouter = new Router();
@@ -11,22 +13,67 @@ const employeesRouter = new Router();
 // Liste over ansatte
 employeesRouter.get("/", (_, res) => {
   const employees = getEmployees();
+
   res.json(employees);
 });
 
 // Hent én spesifikk ansatt
 employeesRouter.get("/:id", (req, res) => {
- const id = Number(req.params.id);
+  const { id } = req.params;
   const employee = getEmployeeById(id);
 
   res.json(employee);
 });
-employeesRouter.post("/", (req, res) =>{
-    const body = req.body
-    const result = createEmployee(body)
 
-    res.status(201).json(result); // 201->created
+// Opprette ny ansatt, uten spesifisert id
+employeesRouter.post("/", (req, res) => {
+  const body = req.body;
+  const result = createEmployee(body);
 
+  res.status(201).json(result); // 201 -> Created
+});
+
+// Oppdatere data for en ansatt
+employeesRouter.patch("/:id", (req, res) => {
+  const { id } = req.params;
+  const { body } = req;
+
+  const result = updateEmployee(id, body);
+
+  res.status(200).json(result);
+});
+
+// Oppdatere data for en ansatt basert på id, hvis id ikke eksisterer,
+// opprett en ansatt med den aktuelle id-en.
+employeesRouter.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { body } = req;
+
+  const result = upsertEmployee(id, body);
+  const statusCode = result.status;
+  delete result.status;
+
+  res.status(statusCode).json(result);
+});
+
+// Sletter en ansatt
+employeesRouter.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const result = deleteEmployee(id);
+
+  const statusCode = result.status;
+
+  res.sendStatus(statusCode);
+});
+
+employeesRouter.use((err, req, res, next) => {
+  console.log("!!!!!!! Error?", err);
+
+  if (req.headersSent) {
+    return;
+  }
+
+  res.status(err.cause).json({ success: false, error: err });
 });
 
 module.exports.employeesRouter = employeesRouter;
